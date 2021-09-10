fc23c8a8231ab5b33f8c419c882318314142b1a2
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2021-09-09 20:22:12 -0600

    Attempt 1 to implement proxy logic

--- client/src/graphql/urql/next-urql-client-config.ts
+++ client/src/graphql/urql/next-urql-client-config.ts
@@ -1,18 +1,29 @@
 import { SSRExchange, NextUrqlClientConfig } from 'next-urql'
 import { dedupExchange, fetchExchange } from 'urql'
 import { NextPageContext } from 'next'
+import absoluteUrl from 'next-absolute-url'
 
 import cacheExchange from './exchanges/cache'
+import isServer from '../../util/common/functions/is-server'
 
-const nextUrqlClientConfig: NextUrqlClientConfig = (ssrExchange: SSRExchange, ctx: NextPageContext) => ({
-  url: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT_URL,
-  fetchOptions: {
-    credentials: 'include',
-    headers: {
-      cookie: ctx && ctx.req ? ctx.req.headers.cookie : null
-    }
-  },
-  exchanges: [dedupExchange, cacheExchange, ssrExchange, fetchExchange]
-})
+const nextUrqlClientConfig: NextUrqlClientConfig = (ssrExchange: SSRExchange, ctx: NextPageContext) => {
+  let url = process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT_URL!
+
+  if (process.env.NEXT_PUBLIC_USES_PROXY === 'true' && isServer()) {
+    const { origin } = absoluteUrl(ctx.req)
+    url = `${origin}${process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT_URL!}`
+  }
+
+  return {
+    url,
+    fetchOptions: {
+      credentials: 'include',
+      headers: {
+        cookie: ctx && ctx.req ? ctx.req.headers.cookie : null
+      }
+    },
+    exchanges: [dedupExchange, cacheExchange, ssrExchange, fetchExchange]
+  }
+}
 
 export default nextUrqlClientConfig
--- client/src/graphql/urql/server/get-urql-client-for-server-side-props.ts
+++ client/src/graphql/urql/server/get-urql-client-for-server-side-props.ts
@@ -2,13 +2,23 @@ import { ssrExchange, dedupExchange, fetchExchange, Client } from 'urql'
 import { initUrqlClient, SSRExchange } from 'next-urql'
 import { GetServerSidePropsContext } from 'next'
 import { ParsedUrlQuery } from 'querystring'
+import absoluteUrl from 'next-absolute-url'
+
 import cacheExchange from '../exchanges/cache'
+import isServer from '../../../util/common/functions/is-server'
 
 const getUrqlClientForServerSideProps = (ctx: GetServerSidePropsContext<ParsedUrlQuery>): [Client, SSRExchange] => {
   const ssrCache = ssrExchange({ isClient: false })
 
+  let url = process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT_URL!
+
+  if (process.env.NEXT_PUBLIC_USES_PROXY === 'true' && isServer()) {
+    const { origin } = absoluteUrl(ctx.req)
+    url = `${origin}${process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT_URL!}`
+  }
+
   const client = initUrqlClient({
-    url: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT_URL,
+    url,
     fetchOptions: {
       credentials: 'include',
       headers: {
