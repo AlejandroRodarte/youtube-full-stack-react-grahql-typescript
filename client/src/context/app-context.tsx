a778eb1ccfb5cda32bee3729ffce247670016c11
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2021-09-16 15:17:18 -0600

    Add new/popular sort options

--- client/src/context/app-context.tsx
+++ client/src/context/app-context.tsx
@@ -4,15 +4,78 @@ import PropTypes from 'prop-types'
 import { Contexts } from '../types/context'
 
 const AppContext = createContext<Contexts.AppContext>({
-  posts: [],
-  setPosts: () => {}
+  pages: {
+    home: {
+      posts: {
+        new: {
+          value: [],
+          set: () => {}
+        },
+        popular: {
+          value: [],
+          set: () => {}
+        }
+      },
+      sort: {
+        value: 'new',
+        set: () => {}
+      },
+      cursors: {
+        new: {
+          value: null,
+          set: () => {}
+        },
+        popular: {
+          value: null,
+          set: () => {}
+        }
+      }
+    }
+  }
 })
 
 export const AppContextWrapper: React.FC = ({ children }) => {
-  const [posts, setPosts] = useState<Contexts.Posts>([])
+  const [sort, setSort] = useState<Contexts.Sort>('new')
+
+  const [newPosts, setNewPosts] = useState<Contexts.Posts>([])
+  const [popularPosts, setPopularPosts] = useState<Contexts.Posts>([])
+
+  const [newCursor, setNewCursor] = useState<string | null>(null)
+  const [popularCursor, setPopularCursor] = useState<string | null>(null)
+
+  const initialContext: Contexts.AppContext = {
+    pages: {
+      home: {
+        posts: {
+          new: {
+            value: newPosts,
+            set: setNewPosts
+          },
+          popular: {
+            value: popularPosts,
+            set: setPopularPosts
+          }
+        },
+        sort: {
+          value: sort,
+          set: setSort
+        },
+        cursors: {
+          new: {
+            value: newCursor,
+            set: setNewCursor
+          },
+          popular: {
+            value: popularCursor,
+            set: setPopularCursor
+          }
+        }
+      }
+    }
+  }
 
   return (
-    <AppContext.Provider value={{ posts, setPosts }}>
+    <AppContext.Provider value={ initialContext }>
       { children }
     </AppContext.Provider>
   )
--- client/src/types/context.ts
+++ client/src/types/context.ts
@@ -3,10 +3,30 @@ import React from 'react'
 import { PostsQuery } from '../generated/graphql'
 
 export namespace Contexts {
+  interface UseStateValues<T> {
+    value: T
+    set: React.Dispatch<React.SetStateAction<T>>
+  }
+
+  type SortConfig<T> = {
+    // eslint-disable-next-line no-unused-vars
+    [key in Sort]: UseStateValues<T>
+  }
+
+  interface HomePageContext {
+    sort: UseStateValues<Sort>
+    posts: SortConfig<Posts>
+    cursors: SortConfig<string | null>
+  }
+
+  interface PagesContext {
+    home: HomePageContext
+  }
+
+  export type Sort = 'new' | 'popular'
   export type Posts = PostsQuery['posts']['data']['posts']
 
   export interface AppContext {
-    posts: Posts
-    setPosts: React.Dispatch<React.SetStateAction<Posts>>
+    pages: PagesContext
   }
 }
